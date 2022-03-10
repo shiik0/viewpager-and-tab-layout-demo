9d85ce0e56d0a061adfa071c5f131dd4f08bbe71
Chico Pereira <olachico@icloud.com>
2022-03-10 14:18:16 +0000

    first commit

---
+++ app/src/main/java/com/example/androidtabs_demo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.androidtabs_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val images = listOf(
+            R.drawable.appalachian1,
+            R.drawable.appalachian2,
+            R.drawable.appalachian3,
+            R.drawable.appalachian4,
+            R.drawable.appalachian5,
+            R.drawable.appalachian6
+        )
+
+        val adapter = ViewPagerAdapter(images)
+
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Reselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Unselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+}
